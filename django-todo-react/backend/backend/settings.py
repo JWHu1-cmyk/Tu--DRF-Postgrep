e092c84bbdcfdba2083e14592c4ef8bbcdece4e0
John Doe <jjhu1999@gmail.com>
2024-08-28 16:58:49 -0700

    finally

--- django-todo-react/backend/backend/settings.py
+++ django-todo-react/backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-kyg@z)w639rpuis*ee!na6)1kgcamr*u1@ol!!l(qz)z(_a0xn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
