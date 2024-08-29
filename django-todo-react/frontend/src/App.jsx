47c2dbadb24ee139b4611c749aa7b495c9310843
John Doe <jjhu1999@gmail.com>
2024-08-29 15:36:36 -0700

    yeah

--- django-todo-react/frontend/src/App.jsx
+++ django-todo-react/frontend/src/App.jsx
@@ -34,7 +34,23 @@ class App extends Component {
     axios
       .get('/api/todos/')
       .then((res) => this.setState({ todoList: res.data }))
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.error("Error fetching todos:", err);
+        // Log more detailed error information
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.error("Response data:", err.response.data);
+          console.error("Response status:", err.response.status);
+          console.error("Response headers:", err.response.headers);
+        } else if (err.request) {
+          // The request was made but no response was received
+          console.error("No response received:", err.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.error("Error setting up request:", err.message);
+        }
+      });
   };
 
   toggle = () => {
